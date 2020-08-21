b800f0c47f3dd88924079e2b0c8e77f51bde186f
Carlos Hernandez <carloshhyh@gmail.com>
2020-08-21 00:22:08 -0500

    Basic Login

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask,request, make_response,redirect,render_template,session
+from flask import Flask,request, make_response,redirect,render_template,session,url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] ='SUPER SECRET'
-
+csrf = CSRFProtect(app)
 todos = ['Estudiar en Platzi', 'Comer', 'Dormir']
+
+
+class LoginForm(FlaskForm):
+	username = StringField('User Name', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	submit = SubmitField('Send')
 
 
 @app.errorhandler(404)
@@ -27,12 +38,23 @@ def index():
 	return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET','POST'])
 def hello():
 	user_ip = session.get('user_ip')
+	login_form = LoginForm()
+	username = session.get('username')
+
 	context = {
 		'user_ip' : user_ip,
 		'todos' : todos,
+		'login_form' : login_form,
+		'username': username
 	}
+
+	if login_form.validate_on_submit():
+		username = login_form.username.data
+		session['username'] = username
+		return redirect(url_for('index'))
+
 	return render_template('hello.html', **context)
 
