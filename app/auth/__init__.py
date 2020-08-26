0831d11c2d070038207340a713d3b255f3b62469
Carlos Hernandez <carloshhyh@gmail.com>
2020-08-26 01:06:41 -0500

    'Blueprints_2'

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
+
+#url_prefix indica que todas las rutas que incien con /auth serán dirigidas a este blueprint
 auth = Blueprint('auth',__name__,url_prefix='/auth')
 
 from . import views
